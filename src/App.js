244f8bc01ee2ae0d107fdb3e00573500500a3797
Pardis-H <pardis.haghdoust@gmail.com>
2023-07-03 12:50:36 +0330

    add notFound Page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { Route, Routes } from "react-router-dom";
 import Users from "./pages/Users";
 import Home from "./pages/Home";
 import Layout from "./pages/Layout";
+import NotFound from "./pages/NotFound";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route path="dashboard/users" element={<Users />} />
         <Route path="/" element={<Home />} />
+        <Route path='*' element={<NotFound/>} />
       </Routes>
     </Layout>
   );
